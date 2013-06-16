bc2218b14f36f02d1810e5d698274a961bfc9527
ermau <me@ermau.com>
2013-06-16 18:07:39 -0400

    Add Group tests

---
+++ Desktop/Tempest.Social.Tests/GroupTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Tempest.Social.Tests
+{
+	[TestFixture]
+	public class GroupTests
+	{
+		[Test]
+		public void CtorInvalid()
+		{
+			Assert.Throws<ArgumentNullException> (() => new Group (1, null));
+		}
+
+		[Test]
+		public void Ctor()
+		{
+			string[] ids = new[] { "foo", "bar" };
+			var g = new Group (1, ids);
+			Assert.AreEqual (1, g.Id);
+			CollectionAssert.AreEqual (ids, g.Participants);
+		}
+	}
+}
